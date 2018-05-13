64b0f440dfcff980c8a5c12af70411ca7f91fa6c
Nikhil Gaikwad <nikhil.gaikwad84@yahoo.ccom>
2018-05-13 21:00:08 +0530

    Blank Project

---
+++ 1ClickMelodyApplication/1ClickMelodyApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1ClickMelodyApplication.Startup))]
+namespace _1ClickMelodyApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
